b4baf76700d47aca757ba24efe4d3f6c639cdb3b
Connor Milligan <connoramilligan@gmail.com>
2022-09-08 18:11:08 -0400

    Basic curl setup

--- main.cpp
+++ main.cpp
@@ -8,19 +8,22 @@ int main() {
     CURLcode res;
 
     curl = curl_easy_init();
+    string token = getenv("GITHUB_TOKEN");
+    struct curl_slist *headers = NULL;
+
     if(curl) {
         curl_easy_setopt(curl, CURLOPT_URL, "https://api.github.com/octocat");
-        /* example.com is redirected, so we tell libcurl to follow redirection */
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-    
-        /* Perform the request, res will get the return code */
+        headers = curl_slist_append(headers, "User-Agent: Talc");
+        headers = curl_slist_append(headers, ("Authorization: Bearer " + token).c_str());
+        
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER,headers);
+
         res = curl_easy_perform(curl);
-        /* Check for errors */
+
         if(res != CURLE_OK)
         fprintf(stderr, "curl_easy_perform() failed: %s\n",
                 curl_easy_strerror(res));
     
-        /* always cleanup */
         curl_easy_cleanup(curl);
     }
     return 0;
